9bc347d7995aa1678510606913999946dc52d262
Fede23gyt <fedesuppa@gmail.com>
2025-07-30 12:50:08 -0300

    Migraciones y Modelos - Nomencladores Tipo_nota, Memos, Tipo_compra_ Tipo_Insumo

---
+++ app/Models/TipoNota.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoNota extends Model
+{
+    protected $table = 'tipo_nota';
+    protected $fillable = ['descripcion', 'estado'];
+}
